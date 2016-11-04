9d261156e5e78758f455675bf0df64ae5a0ac761
davidhickey <davidhickey423@gmail.com>
2016-11-04 14:56:04 -0400

    adds skills to job seeker ne view, working through issues still with js controller

--- app/controllers/job_seekers_controller.rb
+++ app/controllers/job_seekers_controller.rb
@@ -15,6 +15,7 @@ class JobSeekersController < ApplicationController
   # GET /job_seekers/new
   def new
     @job_seeker = JobSeeker.new
+    @skill = Skill.new
   end
 
   # GET /job_seekers/1/edit
@@ -26,15 +27,17 @@ class JobSeekersController < ApplicationController
   def create
     @job_seeker = JobSeeker.new(job_seeker_params)
     @job_seeker = DateTime.now
+    @skill = Skill.new(skill_params)
     @job_seeker.save
 
-    if @job_seeker.save
-      @job_seeker.skills.create skills_params
-      re_direct_to_root_url
-    end
+    # if @job_seeker.save
+    #   @job_seeker.skill.new(skill_params)
+    #   re_direct_to_root_url
+    # end
 
     respond_to do |format|
       if @job_seeker.save
+        @job_seeker.skill.new(skill_params)
         format.html { redirect_to @job_seeker, notice: 'Job seeker was successfully created.' }
         format.json { render :show, status: :created, location: @job_seeker }
       else
@@ -68,6 +71,10 @@ class JobSeekersController < ApplicationController
     end
   end
 
+end
+
+
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_job_seeker
@@ -76,9 +83,11 @@ class JobSeekersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def job_seeker_params
-      params.require(:job_seeker).permit(:username, :password, :first_name, :last_name, :email, :state, :city, :bio, :portfolio, :social_media, :rating, :comments, :interests, :skills)
+      params.require(:job_seeker).permit(:username, :password, :first_name, :last_name, :email, :state, :city, :bio, :portfolio, :social_media, :rating, :comments, :interests, :skill_id)
     end
 
-    def skills_params
-        params.require(:skills).permit(:skill_1, :skill_2, :skill_3, :skill_4)
+    def skill_params
+        params.require(:skill).permit(:skill_1, :skill_2, :skill_3, :skill_4)
+        # params.permit(:skill_1, :skill_2, :skill_3, :skill_4)
+
     end
